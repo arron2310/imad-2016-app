51ba16131064dd265274221a00304539b2204f9a
arron2310 <arron2310@gmail.com>
2016-09-21 21:17:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('Article one has been requested and will be served here!');
+  res.send('Article one has been requested and will be served here!');
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile('Article two has been requested and will be served here!');
+  res.send('Article two has been requested and will be served here!');
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile('Article three has been requested and will be served here!');
+  res.send('Article three has been requested and will be served here!');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
